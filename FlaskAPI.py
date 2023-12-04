42fd98f488f78e71d042e7e86434b2d02966166e
anoopkrishnadas04 <anoopkrishnadasgit@gmail.com>
2023-12-04 16:57:50 -0500

    Add Graph and Table Scripts

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -10,14 +10,15 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def get_stock_info():
     stock = str(request.args.get('stock')).strip().upper()
-    graph = str(request.args.get('graph')).strip().upper()
+    range = str(request.args.get('range')).strip()
     
     input_status = "VALID"
-    print(stock)
+    #print(stock)
     if stock == "NONE" or stock == "":
         input_status = "INVALID"
         return {'input-status' : input_status}
-    
+
+    #ACCESSING STOCK DATA 
     url = f'https://finance.yahoo.com/quote/{stock}?p={stock}'
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0'}
     response = requests.get(url=url, headers=headers)
@@ -44,11 +45,83 @@ def get_stock_info():
             else:
                 market_percentage = row.contents[0]
 
-    graph
-    market_graph = ""
-    if(graph == "TRUE"):
-        market_graph = "YAY"
+    #ACCESSING GRAPH DATA
+    graph_url = ""
+    valid_ranges = ["1d", "5d", "1mo", "6mo", "1y", "5y", "ytd"]
+    for rng in valid_ranges:
+        if rng == range:
+            interval = ""
+            if(range == "1d"):
+                interval = "2m"
+            if(range == "5d"):
+                interval = "15m"
+            if(range == "1mo"):
+                interval = "1h"
+            if(range == "6mo"):
+                interval = "1d"
+            if(range == "1y"):
+                interval = "1d"
+            if(range == "5y"):
+                interval = "1wk"
+            if(range == "ytd"):
+                interval = "3mo"
+            graph_url = f"https://query1.finance.yahoo.com/v8/finance/chart/{stock}?region=US&lang=en-US&includePrePost=false&interval={interval}&useYfid=true&range={range}&corsDomain=finance.yahoo.com&.tsrc=finance"
     
+    if graph_url != "":
+        data_request = Request(graph_url, headers=headers)
+        page = urlopen(data_request)
+        graph_json = page.read().decode("utf-8")
+    
+    #ACCESSING TABLE DATA
+    table_json = {}
+    table_data = soup.find_all('td')
+    table_headers = [
+        "Previous Close",
+        "Open",
+        "Bid",
+        "Ask",
+        "Day's Range",
+        "52 Week Range",
+        "Volume",
+        "Avg. Volume",
+        "Market Cap",
+        "Beta (5Y Monthly)",
+        "PE Ratio (TTM)",
+        "EPS (TTM)",
+        "Earnings Date",
+        "Forward Dividend & Yield",
+        "Ex-Dividend Date",
+        "1y Target Est"
+    ]
+    table_values = {
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+    }
+    counter = 0
+    for td in table_data:
+        if td['class'] == "Ta(end)":
+            table_values[counter] = td.contents
+            print(table_values[counter])
+            counter += 1
+
+    #FIX TABLE
+
+
+    #RETURN JSON
     return {
         'input-status' : input_status,
         'url' : url,
@@ -57,8 +130,14 @@ def get_stock_info():
         'market-change' : market_change,
         'market-percentage' : market_percentage,
         
-        'market-graph' : market_graph
+        'graph-url' : graph_url,
+        'interval' : interval,
+        'range' : range,
+        'graph-json' : graph_json,
+
+        'table-json' : table_json
     }
 
 if __name__ == "__main__":
     app.run(port=5000)
+    #app.run(host="192.168.86.84", port=5000)
